61aabb1be6a578f696b937028145191053a91370
Alwyn Lopez <alwyn333@hotmail.com>
2020-02-07 20:51:34 -0500

    Added filter.py file

---
+++ filter.py
@@ -0,0 +1,89 @@
+from PIL import Image
+from progress.bar import Bar
+import random
+import math
+
+class Cell:
+	def __init__(self, center_point):
+		self.center_point    = center_point
+		self.neighbor_points = []
+		self.cell_color 	 = (0, 0, 0)
+
+	def update_cell_color(self, color):
+		old_r = self.cell_color[0]
+		old_g = self.cell_color[1]
+		old_b = self.cell_color[2]
+
+		num_neighbors = len(self.neighbor_points)
+
+		new_r = (old_r * (num_neighbors - 1) + color[0]) / num_neighbors
+		new_g = (old_g * (num_neighbors - 1) + color[1]) / num_neighbors
+		new_b = (old_b * (num_neighbors - 1) + color[2]) / num_neighbors
+
+		self.cell_color = (
+			round(new_r),
+			round(new_g),
+			round(new_b)
+		)
+
+def generate_voronoi_diagram(num_cells, image_name, distance = "euclidean"):
+	old_img = Image.open(image_name)
+	img_x = old_img.size[0]
+	img_y = old_img.size[1]
+	new_img = Image.new("RGB", (img_x, img_y))
+
+	if distance == 'manhattan':
+		metric = lambda x, a, y, b: math.fabs(x - a) + math.fabs(y - b)
+	else:
+		metric = lambda x, a, y, b: math.hypot(x - a, y - b)
+
+	cells = []
+	bar = Bar("1) Generating cell locations ...", max = num_cells)
+	for _ in range(num_cells):
+		cpx = random.randrange(img_x)
+		cpy = random.randrange(img_y)
+		cp  = (cpx, cpy)
+		cells.append(Cell(cp))
+		bar.next()
+
+	bar.finish()
+
+	ctr_pts = [ cell.center_point for cell in cells ]
+	all_pts = [ (x, y) for x in range(img_x) for y in range(img_y) ]
+	bar = Bar("2) Assigning pixels to a cell ..", max = len(all_pts))
+	for pt in all_pts:
+		x, y  = pt[0], pt[1]
+		ctr_x = ctr_pts[0][0]
+		ctr_y = ctr_pts[0][1]
+
+		min_d = metric(x, ctr_x, y, ctr_y)
+		min_j = 0
+
+		for i in range(1, num_cells):
+			ctr_x = ctr_pts[i][0]
+			ctr_y = ctr_pts[i][1]
+			d = metric(x, ctr_x, y, ctr_y)
+
+			if d < min_d:
+				min_d = d
+				min_j = i
+
+		cells[min_j].neighbor_points.append(pt)
+		cells[min_j].update_cell_color(old_img.getpixel(pt))
+		bar.next()
+
+	bar.finish()
+
+	bar = Bar("3) Coloring the cells ..........", max = len(cells))
+	for cell in cells:
+		color = cell.cell_color
+
+		for neighbor_point in cell.neighbor_points:
+			new_img.putpixel(neighbor_point, color)
+
+		bar.next()
+
+	bar.finish()
+
+	new_img.save(distance + str(num_cells) + "Voronoi" + image_name)
+	new_img.show()
