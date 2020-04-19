d539e5e539af591aac81cb285c5b52c5887ad510
Alwyn Lopez <alwyn333@hotmail.com>
2020-04-19 16:55:14 -0400

    Added batch processing function for image filter

--- voronoi_image_processing/image_filter.py
+++ voronoi_image_processing/image_filter.py
@@ -1,4 +1,5 @@
 import numpy as np
+import os
 import random
 import time
 
@@ -106,3 +107,10 @@ def generate_filtered_image(image, settings = DEFAULT_IMAGE_FILTER_SETTINGS):
 	new_img.save(file_name + "_filtered." + file_type)
 
 	if display_new_image: new_img.show()
+
+def generate_filtered_image_directory(image_directory, settings = DEFAULT_IMAGE_FILTER_SETTINGS):
+	image_list = os.listdir(image_directory)
+	num_images = len(image_list)
+	for i, image in enumerate(image_list):
+		print("Processing %s, %d / %d" % (image, i + 1, num_images))
+		generate_filtered_image(image_directory + image, settings)
